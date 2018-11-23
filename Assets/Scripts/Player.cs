12d8de7768e809ebe01011abd4a5d747e52d0530
Nyyani <43067342+Nyyani@users.noreply.github.com>
2018-11-23 14:50:55 +0000

    something

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Extra using statement to allow us to use the scene management functions
+using UnityEngine.SceneManagement;
+
 public class Player : MonoBehaviour
 {
 
     // designer variables
     public float speed = 10;
+    public float jumpSpeed = 10;
     public Rigidbody2D physicsBody;
     public string horizontalAxis = "Horizontal";
+    public string jumpButton = "Jump";
 
     public Animator playerAnimator;
     public SpriteRenderer playerSprite;
+    public Collider2D playerCollider;
 
     // Use this for initialization
     void Start()
@@ -53,6 +59,37 @@ public class Player : MonoBehaviour
             playerSprite.flipX = false;
         }
 
+        // Jumping
+
+        //Detect if we are touching the ground
+        //Get the LayerMask from Unity using the name of the layer
+        LayerMask groundLayerMask = LayerMask.GetMask("Ground");
+        //Ask the player's collider if we are touching the LayerMask
+        bool touchingGround = playerCollider.IsTouchingLayers(groundLayerMask);
+
+        bool jumpButtonPressed = Input.GetButtonDown(jumpButton);
+        if (jumpButtonPressed == true && touchingGround == true)
+        {
+            //We have pressed Jump so we should set our upward velocity to our jumpSpeed
+            velocity.y = jumpSpeed;
+
+            //Give the velocity to the rigidbody
+            physicsBody.velocity = velocity;
+        }
+
+    }
+
+
+    // Our own function for handling player death
+    public void Kill()
+    {
+        //Reset the current level to restart from the beginning
+        //First, ask unity what the current level is
+
+        Scene currentLevel = SceneManager.GetActiveScene();
+
+        //Second, tell unity to load the current again by passing the build index of our level
+        SceneManager.LoadScene(currentLevel.buildIndex);
 
     }
 
